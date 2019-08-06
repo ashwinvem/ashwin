8517fd2c2826e313a07bbd905e84ebb27890fc54
ashwinvem <51525902+ashwinvem@users.noreply.github.com>
2019-08-06 11:54:16 +0530

    Create positive.cpp

---
+++ Codekata/positive.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int num;
+int main(){
+    cin>>num;
+    if(num<0){
+    cout<<"negative";
+    }
+    else{
+        cout<<"positive";
+    }
+    return 0;
+}
